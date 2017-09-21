cce931f598d60445450c11fa53461842f4a248bf
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2017-09-21 21:32:04 +0300

    Add pad/unpad functions

--- datasets.py
+++ datasets.py
@@ -3,6 +3,20 @@ from params import args
 from sklearn.model_selection import train_test_split, shuffle
 from random_transform_mask import ImageWithMaskFunction
 
+def pad(image, padding_w, padding_h):
+    height, width, depth = image.shape
+    new_image = np.zeros((height + padding_h * 2, width + padding_w * 2, depth), dtype=image.dtype)
+    new_image[padding_h:(height + padding_h), padding_w:(width + padding_w)] = image
+    # @TODO: Fill padded zones
+    # new_image[:, :padding_w] = image[:, :padding_w]
+    # new_image[:padding_h, :] = image[:padding_h, :]
+    # new_image[-padding_h:, :] = image[-padding_h:, :]
+
+    return new_image
+
+def unpad(image, padding_w):
+    return image[:, padding_w:(image.shape[1] - padding_w), :]
+
 def generate_filenames(car_ids):
     return ['{}_{}'.format(id, str(angle + 1).zfill(2)) for angle in range(16) for id in ids_train_split]
 
@@ -26,7 +40,8 @@ def bootstrapped_split(car_ids, seed=args.seed):
     return generate_filenames(bootstrapped_train_ids.values),
            generate_filenames(valid_ids)
 
-def build_batch_generator(filenames, dataset_dir=args.dataset_dir, batch_size=args.batch_size, shuffle=False, transformations=None,
+def build_batch_generator(filenames, dataset_dir=args.dataset_dir, batch_size=args.batch_size,
+                          shuffle=False, transformations=None,
                           out_size=None, crop_size=None, mask_dir=None, aug=False):
     mask_function = ImageWithMaskFunction(out_size=out_size, crop_size=crop_size, mask_dir=mask_dir)
 
