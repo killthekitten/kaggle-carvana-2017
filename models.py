4accac841136e63daccd1ade565125cbedfbc6f5
selim <selim.sef@gmail.com>
2017-09-22 11:38:23 +0200

    added mobilenet unet

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from keras.layers.core import Activation, SpatialDropout2D
 from keras.layers.merge import concatenate
 from keras.layers.normalization import BatchNormalization
 
+from mobile_net_fixed import MobileNet
 from resnet50_fixed import ResNet50
 from params import args
 
@@ -15,6 +16,10 @@ def conv_block_simple(prevlayer, filters, prefix, strides=(1, 1)):
     conv = Activation('relu', name=prefix + "_activation")(conv)
     return conv
 
+"""
+Unet with Mobile net encoder
+Uses caffe preprocessing function
+"""
 def get_unet_resnet(input_shape):
     resnet_base = ResNet50(input_shape=input_shape, include_top=False)
     
@@ -55,4 +60,40 @@ def get_unet_resnet(input_shape):
     conv10 = SpatialDropout2D(0.2)(conv10)
     x = Conv2D(1, (1, 1), activation="sigmoid", name="prediction")(conv10)
     model = Model(resnet_base.input, x)
+    return model
+
+"""
+Unet with Mobile net encoder
+Uses the same preprocessing as in Inception, Xception etc. (imagenet_utils.preprocess_input with mode 'tf' in new Keras version)
+"""
+def get_unet_mobilenet(input_shape):
+    base_model = MobileNet(include_top=False, input_shape=input_shape)
+
+    conv1 = base_model.get_layer('conv_pw_1_relu').output
+    conv2 = base_model.get_layer('conv_pw_3_relu').output
+    conv3 = base_model.get_layer('conv_pw_5_relu').output
+    conv4 = base_model.get_layer('conv_pw_11_relu').output
+    conv5 = base_model.get_layer('conv_pw_13_relu').output
+    up6 = concatenate([UpSampling2D()(conv5), conv4], axis=-1)
+    conv6 = conv_block_simple(up6, 256, "conv6_1")
+    conv6 = conv_block_simple(conv6, 256, "conv6_2")
+
+    up7 = concatenate([UpSampling2D()(conv6), conv3], axis=-1)
+    conv7 = conv_block_simple(up7, 256, "conv7_1")
+    conv7 = conv_block_simple(conv7, 256, "conv7_2")
+
+    up8 = concatenate([UpSampling2D()(conv7), conv2], axis=-1)
+    conv8 = conv_block_simple(up8, 192, "conv8_1")
+    conv8 = conv_block_simple(conv8, 128, "conv8_2")
+
+    up9 = concatenate([UpSampling2D()(conv8), conv1], axis=-1)
+    conv9 = conv_block_simple(up9, 96, "conv9_1")
+    conv9 = conv_block_simple(conv9, 64, "conv9_2")
+
+    up10 = concatenate([UpSampling2D()(conv9), base_model.input], axis=-1)
+    conv10 = conv_block_simple(up10, 48, "conv10_1")
+    conv10 = conv_block_simple(conv10, 32, "conv10_2")
+    conv10 = SpatialDropout2D(0.2)(conv10)
+    x = Conv2D(1, (1, 1), activation="sigmoid", name="prediction")(conv10)
+    model = Model(base_model.input, x)
     return model
